dbc21c495d3f07ba30b2662b9bf5ccd248f8dca1
Jérôme Crepin <jerome.crepin@outlook.com>
2020-08-24 18:56:07 +0200

    Start of the exercice

---
+++ lib/input_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+const double bottomContainerHeight = 80.0;
+const Color activeCardColor = Color(0xFF1D1E33);
+const Color bottomContainerColor = Color(0xFFEB1555);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  ReusableCard(color: activeCardColor),
+                  ReusableCard(color: activeCardColor),
+                ],
+              ),
+            ),
+            ReusableCard(color: activeCardColor),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  ReusableCard(color: activeCardColor),
+                  ReusableCard(color: activeCardColor),
+                ],
+              ),
+            ),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  ReusableCard({@required this.color});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+}
